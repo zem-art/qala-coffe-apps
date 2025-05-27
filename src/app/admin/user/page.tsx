4c91e31d9bad3fa9f9f87a223c6b3286e12e148e
zaid <amonsefra13@gmail.com>
2025-05-27 23:48:13 +0700

    feat: add user page component for admin section

---
+++ src/app/admin/user/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from 'react';
+
+export default function UserPage() {
+  return (
+    <>
+        <a className="">
+            this is user
+        </a>
+    </>
+  );
+}
