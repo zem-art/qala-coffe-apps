d7254ab8118d8a7e9e9762eb6268409b4b1ed623
zaid <amonsefra13@gmail.com>
2025-05-29 01:04:15 +0700

    fix: update DATABASE_URL in .env.example and enhance product form submission in AdminMenuPage

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -4,15 +4,17 @@ import React, { useState } from 'react';
 import { api } from "~/trpc/react";
 
 export default function AdminMenuPage() {
-    const utils = api.useUtils();
-//   const createProduct = api.product.create.useMutation({
-//     onSuccess: () => {
-//       utils.invalidate();
-//       alert("Product created!");
-//     },
-//   });
+  const utils = api.useUtils();
+  const createProduct = api.product.create.useMutation({
+    onSuccess: () => {
+      utils.invalidate();
+      setTimeout(() => {
+        window.location.reload()
+      }, 1000);
+    },
+  });
 
-  // const { data: categories } = api.product.getCategories.useQuery();
+  const { data: categories } = api.product.getCategories.useQuery();
 
   const [form, setForm] = useState({
     name: "",
@@ -26,47 +28,47 @@ export default function AdminMenuPage() {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
-  const categories = [
-    { id : 1, name : "late"},
-    { id : 2, name : "americano"},
-    { id : 3, name : "capuchino"},
-    { id : 4, name : "choco"},
-  ]
-
-//   const handleSubmit = (e: React.FormEvent) => {
-//     e.preventDefault();
-//     createProduct.mutate({
-//       name: form.name,
-//       description: form.description,
-//       price: parseFloat(form.price),
-//       imageUrl: form.imageUrl,
-//       categoryId: parseInt(form.categoryId),
-//     });
-//   };
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    createProduct.mutate({
+      name: form.name,
+      description: form.description,
+      price: parseFloat(form.price),
+      imageUrl: form.imageUrl,
+      categoryId: parseInt(form.categoryId),
+    });
+  };
 
   return (
     <div className="">
-      <h2 className="text-2xl font-bold mb-4 dark:text-background">Add Coffee</h2>
+      <div className='flex items-center justify-between pb-4'>
+        <h2 className="text-2xl font-bold mb-4 dark:text-background uppercase">add coffee</h2>
+        <button className='p-2 rounded-sm bg-secondary hover:bg-accent cursor-pointer'>
+          <a className='text-black dark:text-background uppercase'>
+            add category
+          </a>
+        </button>
+      </div>
       <form 
-        // onSubmit={handleSubmit} 
+        onSubmit={handleSubmit}
         className="space-y-4 text-sm">
         <input
-            name="name"
-            placeholder="Name"
-            value={form.name}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded bg-white text-black dark:bg-gray-800 dark:text-white dark:border-gray-600"
-            required
+          name="name"
+          placeholder="Name"
+          value={form.name}
+          onChange={handleChange}
+          className="w-full p-2 border border-gray-300 rounded bg-white text-black dark:bg-gray-800 dark:text-white dark:border-gray-600"
+          required
         />
         <input
-            name="price"
-            placeholder="Price"
-            value={form.price}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded bg-white text-black dark:bg-gray-800 dark:text-white dark:border-gray-600"
-            required
-            type="number"
-            step="0.01"
+          name="price"
+          placeholder="Price"
+          value={form.price}
+          onChange={handleChange}
+          className="w-full p-2 border border-gray-300 rounded bg-white text-black dark:bg-gray-800 dark:text-white dark:border-gray-600"
+          required
+          type="number"
+          step="0.01"
         />
         {/* <input
             name="imageUrl"
@@ -76,34 +78,34 @@ export default function AdminMenuPage() {
             className="w-full p-2 border border-gray-300 rounded bg-white text-black dark:bg-gray-800 dark:text-white dark:border-gray-600"
         /> */}
         <textarea
-            name="description"
-            placeholder="Description"
-            value={form.description}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded bg-white text-black dark:bg-gray-800 dark:text-white dark:border-gray-600"
+          name="description"
+          placeholder="Description"
+          value={form.description}
+          onChange={handleChange}
+          className="w-full p-2 border border-gray-300 rounded bg-white text-black dark:bg-gray-800 dark:text-white dark:border-gray-600"
         />
 
         {/* Dropdown Category */}
         <select
-            name="categoryId"
-            value={form.categoryId}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded bg-white text-black dark:bg-gray-800 dark:text-white dark:border-gray-600"
-            required
+          name="categoryId"
+          value={form.categoryId}
+          onChange={handleChange}
+          className="w-full p-2 border border-gray-300 rounded bg-white text-black dark:bg-gray-800 dark:text-white dark:border-gray-600"
+          required
         >
-            <option value="" disabled>Select Category</option>
-            {categories?.map((cat) => (
-              <option key={cat.id} value={cat.id}>
-                  {cat.name?.toUpperCase()}
-              </option>
-            ))}
+          <option value="" disabled>Select Category</option>
+          {categories?.map((cat) => (
+            <option key={cat.id} value={cat.id}>
+              {cat.name?.toUpperCase()}
+            </option>
+          ))}
         </select>
 
         <button
             type="submit"
-            className="w-full bg-secondary text-white py-2 rounded hover:bg-accent transition disabled:opacity-50 cursor-pointer"
+            className="w-full bg-secondary text-white p-2 rounded hover:bg-accent transition disabled:opacity-50 cursor-pointer uppercase"
         >
-            Save Product
+            save product
         </button>
         </form>
     </div>
